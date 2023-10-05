e9696302a0186fa93beeeb456448add7a546db81
имя фамилия < >
2023-10-05 17:33:57 +0300

    new commit

--- page_analyzer/html_check.py
+++ page_analyzer/html_check.py
@@ -9,15 +9,16 @@ def get_pars_html(url):
               'description': None}
     try:
         get_status = requests.get(url).status_code
-        request = requests.get(url)
-        soup = BeautifulSoup(request.content, 'html5lib')
-        result['get_status'] = get_status
-        result['head'] = soup.h1.string if soup.h1 else ''
-        result['title'] = soup.title.string if soup.title else ''
-        for link in soup.find_all('meta'):
-            if link.get('name') == 'description':
-                result['description'] = link.get('content')
-                break
+        if get_status == 200:
+            request = requests.get(url)
+            soup = BeautifulSoup(request.content, 'html5lib')
+            result['get_status'] = get_status
+            result['head'] = soup.h1.string if soup.h1 else ''
+            result['title'] = soup.title.string if soup.title else ''
+            for link in soup.find_all('meta'):
+                if link.get('name') == 'description':
+                    result['description'] = link.get('content')
+                    break
     except requests.exceptions.ConnectionError:
         pass
 
